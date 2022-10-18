a117be001a7d62dab9c51b6653b17cac7e2ad516
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-18 14:05:34 +0500

    asd

--- build_func08.py
+++ build_func08.py
@@ -1,2 +1,6 @@
 # the task condition is placed in the README file
+x = 7
+y = 1
+answer = 5*x**2*y**3+x*y**2
+print(answer)
 # https://github.com/codeschool43/Build_in_function_homework#build_func08
