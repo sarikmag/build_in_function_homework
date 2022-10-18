b4621f0659c425322dfd4c8165ec99069840df53
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-18 14:05:43 +0500

    asd

--- build_func10.py
+++ build_func10.py
@@ -1,2 +1,7 @@
 # the task condition is placed in the README file
+x = 8
+y = 4
+answer = 3*pow(y, 1/2)+pow(x, 2/3)
+round(answer, 2)
+print(answer)
 # https://github.com/codeschool43/Build_in_function_homework#build_func10
