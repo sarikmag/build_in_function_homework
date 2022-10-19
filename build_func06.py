3fcfc0b53b5cf4cdb022196d35761f69b7250f60
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-19 11:12:36 +0500

    ads

--- build_func06.py
+++ build_func06.py
@@ -1,5 +1,5 @@
 # the task condition is placed in the README file
 a = 3.456
 answer = round(a, 2)
-print(answer)
+print(answer) 
 # https://github.com/codeschool43/Build_in_function_homework#build_func06
