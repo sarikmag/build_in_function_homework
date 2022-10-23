23253465745806b012e8c17be0c65eb7d031305d
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-23 14:35:10 +0500

    ads

--- build_func02.py
+++ build_func02.py
@@ -1,4 +1,4 @@
 # the task condition is placed in the README file
-answer = 3*(7/5-9/4)
+answer = 3*((7/5)-(9/4))
 print(answer) 
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
